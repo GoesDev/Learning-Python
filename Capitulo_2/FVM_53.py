75be11b112ad2971e18952434765f03a2c23b26f
Goes Dev <jcgoes.dev@gmail.com>
2023-05-11 19:08:23 -0300

    Day 1

---
+++ Capitulo_2/FVM_53.py
@@ -0,0 +1,13 @@
+# 2.1 - Mensagem Simples: Armazene uma mensagem em uma variável
+# e, em seguida, exiba essa mensagem.
+message = "Hello World!"
+print(message)
+
+# 2.2 - Mensagens Simples: Armazene uma mensagem em uma variável
+# e, em seguida, exiba essa mensagem. Então altere o valor de sua
+# variável para uma nova mensagem e mostre essa nova mensagem.
+
+message = "Hello World!"
+print(message)
+message = "Hello Python Crash Course World!"
+print(message)
