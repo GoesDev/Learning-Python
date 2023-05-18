307600e6601f3b35de8fb41f06469fcf5c4bd9ba
Goes Dev <jcgoes.dev@gmail.com>
2023-05-18 20:45:33 -0300

    finalizando o capitulo 2

---
+++ Capitulo_2/Faca-Voce-Mesmo/FVM_65[2.9_2.8].py
@@ -0,0 +1,15 @@
+# 2.8 - Número oito: Escreva operações de adição, subtração, multiplicação, e
+# divisão que resultem no número 8. Lembre-se de colocar suas operações em
+# instruções print para ver os resultados.
+
+print(4+4)
+print(2*4)
+print(10-2)
+print(40/5)
+
+# 2.9 - Número favorito: Armazene seu número favorito em uma variável. Em
+# seguida, usando essa variável, crie uma mensagem que revele o seu número
+# favorito. Exiba essa mensagem.
+
+number = 25
+print(f"My favorite number is {number}")
