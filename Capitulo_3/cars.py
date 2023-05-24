7e82abbc3e145cac8c251b12330767e8a7ac0ef4
Goes Dev <jcgoes.dev@gmail.com>
2023-05-24 18:23:23 -0300

    outra atividade

---
+++ Capitulo_3/cars.py
@@ -0,0 +1,19 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# sort()
+# cars.sort()
+# cars.sort(reverse=True)
+# print(cars)
+
+# sorted()
+# print(f"Here is the original list: {cars}")
+# print(f"Here is the sorted list: {sorted(cars)}")
+# print(f"Here is the original list again: {cars}")
+
+# reverse()
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+# len()
+# print(len(cars))
