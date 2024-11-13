7ca8db741b6649ba810d7e1917d1b0c1b2440f5e
Goes Dev <jcgoes.dev@gmail.com>
2024-11-13 20:47:11 -0300

    more ativities

---
+++ Python Crash Course/Capitulo_8/Faca-Voce-Mesmo/FVM_195[8.15].py
@@ -0,0 +1,6 @@
+"""8.15 Impressão de modelos: Coloque as funções do exemplo print_models.py
+em um arquivo separado de nome printing_functions.py. Escreva uma instrução
+import no início de print_models.py e modifique o arquivo para usar as funções
+importadas."""
+
+# DONE
---
+++ Python Crash Course/Capitulo_8/Faca-Voce-Mesmo/FVM_195[8.17].py
@@ -0,0 +1,3 @@
+"""8.17 Estilizando funções: Escolha quaisquer três programas que você escreveu
+neste capítulo e garanta que estejam de acordo com as diretrizes de estilo
+descritas nesta seção."""
