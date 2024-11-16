b966bc457a2acc8142852ce31b11ee9de61a0099
Goes Dev <jcgoes.dev@gmail.com>
2024-11-16 16:47:04 -0300

    iniciando capítulo 10

--- Python Crash Course/Capítulo_9/file_reader.py
+++ Python Crash Course/Capítulo_9/file_reader.py
@@ -1,3 +1,10 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents)
+file_name = 'Python Crash Course/Capítulo_9/pi_digits.txt'
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
